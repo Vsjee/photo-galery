47590480b1add41f879f122eee3f1422b8cf7a26
david hernandez <davidfelipehernandez1@gmail.com>
2022-12-31 18:55:10 -0500

    init comp

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+})
+export class FooterComponent {}
