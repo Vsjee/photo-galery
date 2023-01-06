9207b4ec4208d9f915a8f688269046b89fa8068c
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-05 19:58:44 -0500

    add auth.service

---
+++ src/app/services/AuthService/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  Auth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+} from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private afAuth: Auth) {}
+
+  async register(email: string | any, password: string | any) {
+    const user = await createUserWithEmailAndPassword(
+      this.afAuth,
+      email,
+      password
+    );
+    return await signInWithEmailAndPassword(this.afAuth, email, password);
+  }
+
+  logIn(email: string | any, password: string | any) {
+    return signInWithEmailAndPassword(this.afAuth, email, password);
+  }
+
+  logout() {
+    return signOut(this.afAuth);
+  }
+}
