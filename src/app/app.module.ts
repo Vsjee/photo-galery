ff4782a8f9398cf429bf343ea2eaee43bbe8ecc7
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-05 20:40:02 -0500

    add basic firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,12 @@ import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { environment } from '../enviroments/enviroment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { AuthGuardModule } from '@angular/fire/auth-guard';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -17,6 +23,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     NavbarComponent,
     StoreModule.forRoot({}, {}),
     BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    AuthGuardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
