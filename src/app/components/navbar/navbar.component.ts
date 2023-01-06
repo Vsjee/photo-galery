37ec2874091946e1959e84765ed42efdeb079760
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-05 20:37:19 -0500

    add render items depends on route

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+  Event,
+} from '@angular/router';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+
 import { AppRoutingModule } from 'src/app/app-routing.module';
-import { publicRoutes } from 'src/app/models';
+import { privateRoutes, publicRoutes } from 'src/app/models';
+import { AuthService } from 'src/app/services';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, AppRoutingModule],
+  imports: [
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    AppRoutingModule,
+    CommonModule,
+  ],
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-  routes = publicRoutes;
+  showPrivateRoute: boolean = false;
+
+  publicRoutes = publicRoutes;
+  privateRoutes = privateRoutes;
+
+  constructor(private auth: AuthService, private route: Router) {
+    this.route.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart) {
+        console.log('Route change detected');
+      }
+      if (event instanceof NavigationEnd) {
+        if (event.url === `/${privateRoutes.UPLOADIMAGESDASHBOARD}`) {
+          this.showPrivateRoute = true;
+        } else {
+          this.showPrivateRoute = false;
+        }
+      }
+      if (event instanceof NavigationError) {
+        console.error(event.error);
+      }
+    });
+  }
+
+  logout() {
+    this.auth.logout();
+    this.route.navigate([`/${publicRoutes.HOME}`]);
+  }
 }
