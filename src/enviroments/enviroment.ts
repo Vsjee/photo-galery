4c88fa86d8400a7e6e97d26014f2e89227afcab0
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-05 19:55:59 -0500

    add angularfire

---
+++ src/enviroments/enviroment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC7UZjo9IDdrP_sEZ6XBQWjfYVP9myZHwc',
+    authDomain: 'store-images-app.firebaseapp.com',
+    projectId: 'store-images-app',
+    storageBucket: 'store-images-app.appspot.com',
+    messagingSenderId: '630465520307',
+    appId: '1:630465520307:web:264edf7c8ff2eda20886c6',
+  },
+};
