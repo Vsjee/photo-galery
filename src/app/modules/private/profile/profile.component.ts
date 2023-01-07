516ea573594941a46f059079ab6e79fd82a73cb3
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-07 16:55:01 -0500

    use observer to get data from the user

--- src/app/modules/private/profile/profile.component.ts
+++ src/app/modules/private/profile/profile.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  user$ = this.auth.currentActiveUser$.pipe(
+    filter((state) => (state ? true : false))
+  );
+
+  constructor(private auth: AuthService) {}
+}
