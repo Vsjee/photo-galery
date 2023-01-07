b5bf881ed9eb9f4962d730eec4e8388b06cbb332
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-06 20:00:02 -0500

    remove upload module

--- src/app/modules/upload-images-dashboard/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './upload-images-dashboard-routing.module';
-export * from './upload-images-dashboard.component';
-export * from './upload-images-dashboard.module';
--- src/app/modules/upload-images-dashboard/upload-images-dashboard.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-upload-images-dashboard',
-  templateUrl: './upload-images-dashboard.component.html',
-  styleUrls: ['./upload-images-dashboard.component.scss'],
-})
-export class UploadImagesDashboardComponent {}
