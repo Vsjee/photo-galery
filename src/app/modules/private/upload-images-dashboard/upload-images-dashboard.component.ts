a6d5f5d93b6fea75221a5172f85d3dae84cde376
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-07 19:25:42 -0500

    add save and retrive data from firebase

--- src/app/modules/private/upload-images-dashboard/upload-images-dashboard.component.ts
+++ src/app/modules/private/upload-images-dashboard/upload-images-dashboard.component.ts
@@ -1,8 +1,54 @@
 import { Component } from '@angular/core';
+import {
+  Storage,
+  ref,
+  uploadBytes,
+  listAll,
+  getDownloadURL,
+} from '@angular/fire/storage';
 
 @Component({
   selector: 'app-upload-images-dashboard',
   templateUrl: './upload-images-dashboard.component.html',
   styleUrls: ['./upload-images-dashboard.component.scss'],
 })
-export class UploadImagesDashboardComponent {}
+export class UploadImagesDashboardComponent {
+  images: string[];
+
+  constructor(private storage: Storage) {
+    this.images = [];
+  }
+
+  ngOnInit() {
+    this.getImages();
+  }
+
+  uploadImage($event: any) {
+    const file = $event.target.files[0];
+    console.log(file);
+
+    const imgRef = ref(this.storage, `images/${file.name}`);
+
+    uploadBytes(imgRef, file)
+      .then((response) => {
+        console.log(response);
+        this.getImages();
+      })
+      .catch((error) => console.log(error));
+  }
+
+  getImages() {
+    const imagesRef = ref(this.storage, 'images');
+
+    listAll(imagesRef)
+      .then(async (response) => {
+        console.log(response);
+        this.images = [];
+        for (let item of response.items) {
+          const url = await getDownloadURL(item);
+          this.images.push(url);
+        }
+      })
+      .catch((error) => console.log(error));
+  }
+}
