8788ee4a32e7dbafd051f18604127f8941c229c7
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-09 12:31:51 -0500

    use password validation

--- src/app/modules/sign-in/sign-in.component.ts
+++ src/app/modules/sign-in/sign-in.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from 'src/app/services';
+import { AuthService, ValidationService } from 'src/app/services';
 import { Router } from '@angular/router';
 import { privateRoutes } from 'src/app/models';
 
@@ -10,13 +10,23 @@ import { privateRoutes } from 'src/app/models';
   styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent {
-  form = new FormGroup({
-    email: new FormControl('', [Validators.email, Validators.required]),
-    password: new FormControl('', [Validators.required]),
-    confirmPassword: new FormControl('', [Validators.required]),
-  });
+  form = new FormGroup(
+    {
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      confirmPassword: new FormControl(
+        '',
+        Validators.compose([Validators.required])
+      ),
+    },
+    this.validation.passwordMatch('password', 'confirmPassword')
+  );
 
-  constructor(private auth: AuthService, private router: Router) {}
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private validation: ValidationService
+  ) {}
 
   signIn() {
     if (this.form.valid) {
