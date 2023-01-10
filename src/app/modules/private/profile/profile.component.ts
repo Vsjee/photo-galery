fef73bbb3015d6d5574e0959bee7f49eb3721a1f
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-10 12:04:11 -0500

    add logic and conf for open the dialog

--- src/app/modules/private/profile/profile.component.ts
+++ src/app/modules/private/profile/profile.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { AuthService } from 'src/app/services';
 import { filter } from 'rxjs';
 
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { DeleteAccountDialogComponent } from './components';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -12,5 +15,21 @@ export class ProfileComponent {
     filter((state) => (state ? true : false))
   );
 
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private dialog: MatDialog) {}
+
+  openDialog() {
+    const dialogConf = new MatDialogConfig();
+
+    dialogConf.disableClose = true;
+    dialogConf.autoFocus = true;
+
+    const dialogRef = this.dialog.open(
+      DeleteAccountDialogComponent,
+      dialogConf
+    );
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
