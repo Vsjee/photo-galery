b079337f56c59c2d3f7411c1c090b77e19eef840
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-10 12:05:41 -0500

    add buttonModule

--- src/app/modules/private/profile/index.ts
+++ src/app/modules/private/profile/index.ts
@@ -1,3 +1,5 @@
+export * from './components/delete-account-dialog/delete-account-dialog.component';
+export * from './components';
 export * from './profile-routing.module';
 export * from './profile.component';
 export * from './profile.module';
--- src/app/modules/private/profile/profile.module.ts
+++ src/app/modules/private/profile/profile.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ProfileComponent } from './profile.component';
 import { ProfileRoutingModule } from './profile-routing.module';
 
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [ProfileComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ProfileRoutingModule, MatButtonModule],
 })
 export class ProfileModule {}
