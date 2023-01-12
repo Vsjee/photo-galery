63d829789df7f856e4f750038be34023d414d4d8
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-12 15:09:29 -0500

    add new route to my private routing

--- src/app/modules/private/private-entry/private-routing.module.ts
+++ src/app/modules/private/private-entry/private-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { privateRoutes } from 'src/app/models';
+import { FavoritesComponent } from '../favorites';
 import { ProfileComponent } from '../profile';
 import { UploadImagesDashboardComponent } from '../upload-images-dashboard';
 
@@ -15,6 +16,7 @@ const routes: Routes = [
     component: UploadImagesDashboardComponent,
   },
   { path: `${privateRoutes.PROFILE}`, component: ProfileComponent },
+  { path: `${privateRoutes.FAVORITES}`, component: FavoritesComponent },
 ];
 
 @NgModule({
